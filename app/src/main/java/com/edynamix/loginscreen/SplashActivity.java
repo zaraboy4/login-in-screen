b9215b82baf71ab2f152bf03a4085324c401f622
zaraboy4 <yoan.yordanov@yahoo.com>
2026-06-26 19:12:49 +0300

    ready project

--- app/src/main/java/com/edynamix/loginscreen/SplashActivity.java
+++ app/src/main/java/com/edynamix/loginscreen/SplashActivity.java
@@ -18,6 +18,12 @@ public class SplashActivity extends AppCompatActivity {
 
         SharedPreferences prefBoolean = getApplicationContext().getSharedPreferences("BooleanPref", MODE_PRIVATE);
 
+        /*SharedPreference sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putBoolean("key", true);
+        editor.apply();*/
+        //Code to log out button in main navigation
+
         if (prefBoolean.getBoolean("isLoggedIn", false)) {
             //reference to main navigation
         } else {
